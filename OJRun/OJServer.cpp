cac9e274779e2ca3ccf540943c166df771173ea3
srbLinux <3579105336@qq.com>
2024-03-01 00:24:05 -0800

    deleted:    OJRun/OJProcessPool.cpp 	deleted:    OJRun/OJProcessPool.h 	deleted:    OJRun/OJProcessPoolPrivate.cpp 	deleted:    OJRun/OJProcessPoolPrivate.h 	modified:   OJRun/OJServer.cpp

--- OJRun/OJServer.cpp
+++ OJRun/OJServer.cpp
@@ -1,7 +1,6 @@
 #include "OJServer.h"
 
 #include "../OJSql/OJSql.h"
-#include "./OJProcessPool.h"
 
 #if __linux__
 
@@ -16,11 +15,9 @@ public:
     OJServerPrivate(int num, judge_server_result register_func);
 private:
     OJSqlConnector connector;
-    OJProcessPool processPool;
 };
 
 OJServerPrivate::OJServerPrivate(int num, judge_server_result register_callback)
-    : processPool(num, register_callback)
 {
     connector = OJSqlConnector("OJMYSQL");
     connector.setPassword("123"); connector.setUserName("oj_manager");
@@ -35,7 +32,7 @@ OJServer::OJServer(int num, judge_server_result resultRegisterFunc)
 
 void OJServer::join(const std::vector<std::string>& code, OJJudgeLanguage lang, int qid, int uid)
 {
-    vptr->processPool.join(code, uid, qid, lang);
+    
 }
 
 OJ_NAMESPACE_END
